235cce4e8094a2cb0db349e1493bf5a5865748dd
Dogukan Arslan <dogukan2arslan@hotmail.com>
2022-08-23 21:23:27 +0300

    Setup project

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { HashRouter, Switch, Route, Link } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HashRouter>
+      <div className="App">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+            <li>
+              <Link to="/create-user">Create</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/users">Users</Route>
+          <Route path="/create-user">Create User</Route>
+        </Switch>
+      </div>
+    </HashRouter>
   );
 }
 
